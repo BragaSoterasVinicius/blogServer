4677eac335f6122e9683212467a5ca3b31699fde
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-04-17 21:12:29 -0300

    enviroment de desenvolvimento e produção definido

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+});
+
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog_posts'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
